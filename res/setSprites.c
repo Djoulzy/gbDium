07a8c49a847cda60f7f8bfb3d62d668380824092
Julien Marusi <julien.marusi@bbee.fr>
2022-11-02 15:08:53 +0100

    move

--- res/setSprites.c
+++ res/setSprites.c
@@ -5,27 +5,27 @@
 #include "Ship_16x16.h"
 
 const metasprite_t ship_normal[] = {
-    METASPR_ITEM(0, 0, 0, 0), METASPR_ITEM(0, 8, 2, 0),
+    METASPR_ITEM(0, -8, 0, 0), METASPR_ITEM(0, 8, 2, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up1[] = {
-    METASPR_ITEM(0, 0, 4, 0), METASPR_ITEM(0, 8, 6, 0),
+    METASPR_ITEM(0, -8, 4, 0), METASPR_ITEM(0, 8, 6, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up2[] = {
-    METASPR_ITEM(0, 0, 8, 0), METASPR_ITEM(0, 8, 10, 0),
+    METASPR_ITEM(0, -8, 8, 0), METASPR_ITEM(0, 8, 10, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up3[] = {
-    METASPR_ITEM(0, 0, 12, 0), METASPR_ITEM(0, 8, 14, 0),
+    METASPR_ITEM(0, -8, 12, 0), METASPR_ITEM(0, 8, 14, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up4[] = {
-    METASPR_ITEM(0, 0, 16, 0), METASPR_ITEM(0, 8, 18, 0),
+    METASPR_ITEM(0, -8, 16, 0), METASPR_ITEM(0, 8, 18, 0),
     METASPR_TERM
 };
 
--- src/main.c
+++ src/main.c
@@ -24,31 +24,48 @@ void init_gfx() {
 
 void main(void)
 {
-    uint16_t ShipX = 70, ShipY = 70;
+    uint16_t ShipX, ShipY, inclinaison;
+    int16_t SpdX, SpdY, direction;
     uint8_t hiwater = 0;
 
 	init_gfx();
 
     joypad_init(1, &joypads);
+
+    ShipX = ShipY = 64 << 4;
+    SpdX = SpdY = direction = 0;
+
     // Loop forever
     while(1) {
 		// Game main loop processing goes here
 
         joypad_ex(&joypads);
         if (joypads.joy0 & J_UP) {
-            ShipY--;
+            // SpdY -= 2;
+            // if (SpdY < -32) SpdY = -32;
         } else if (joypads.joy0 & J_DOWN) {
-            ShipY++;
+            // SpdY += 2;
+            // if (SpdY > 32) SpdY = 32;
         }
 
         if (joypads.joy0 & J_LEFT) {
-            ShipX--;
+            SpdX -= 2;
+            if (SpdX < -32) SpdX = -32;
+            direction = -1;
         } else if (joypads.joy0 & J_RIGHT) {
-            ShipX++;
+            SpdX += 2;
+            if (SpdX > 32) SpdX = 32;
+            direction = 1;
         }
+        ShipX += SpdX, ShipY += SpdY;
+
     
-        hiwater = move_metasprite(ship_meta[0], 0, SHIP_SPRITE, ShipX, ShipY);
-        hide_sprites_range(hiwater, 40);  
+
+        if (direction >= 0)
+            hiwater = move_metasprite(ship_meta[0], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+        else
+            hiwater = move_metasprite_vflip(ship_meta[0], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+        // hide_sprites_range(hiwater, 40);  
 
 		// Done processing, yield CPU and wait for start of next frame
         wait_vbl_done();
