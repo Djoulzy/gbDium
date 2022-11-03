207419889381eb9f782b22f869a0b033d6571a73
Julien Marusi <secure@marusi.fr>
2022-11-03 08:28:53 +0100

    Shhot

--- res/setSprites.c
+++ res/setSprites.c
@@ -5,27 +5,27 @@
 #include "Ship_16x16.h"
 
 const metasprite_t ship_normal[] = {
-    METASPR_ITEM(0, -8, 0, 0), METASPR_ITEM(0, 8, 2, 0),
+    METASPR_ITEM(0, -8, 0, 0), METASPR_ITEM(8, 0, 1, 0), METASPR_ITEM(-8, 8, 2, 0), METASPR_ITEM(8, 0, 3, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up1[] = {
-    METASPR_ITEM(0, -8, 4, 0), METASPR_ITEM(0, 8, 6, 0),
+    METASPR_ITEM(0, -8, 4, 0), METASPR_ITEM(8, 0, 5, 0), METASPR_ITEM(-8, 8, 6, 0), METASPR_ITEM(8, 0, 7, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up2[] = {
-    METASPR_ITEM(0, -8, 8, 0), METASPR_ITEM(0, 8, 10, 0),
+    METASPR_ITEM(0, -8, 8, 0), METASPR_ITEM(8, 0, 9, 0), METASPR_ITEM(-8, 8, 10, 0), METASPR_ITEM(8, 0, 11, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up3[] = {
-    METASPR_ITEM(0, -8, 12, 0), METASPR_ITEM(0, 8, 14, 0),
+    METASPR_ITEM(0, -8, 12, 0), METASPR_ITEM(8, 0, 13, 0), METASPR_ITEM(-8, 8, 14, 0), METASPR_ITEM(8, 0, 15, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_up4[] = {
-    METASPR_ITEM(0, -8, 16, 0), METASPR_ITEM(0, 8, 18, 0),
+    METASPR_ITEM(0, -8, 16, 0), METASPR_ITEM(8, 0, 17, 0), METASPR_ITEM(-8, 8, 18, 0), METASPR_ITEM(8, 0, 19, 0),
     METASPR_TERM
 };
 
@@ -34,32 +34,32 @@ const metasprite_t* const ship_meta[5] = {
 };
 
 const metasprite_t ship_retournement1[] = {
-    METASPR_ITEM(0, -8, 20, 0), METASPR_ITEM(0, 8, 22, 0),
+    METASPR_ITEM(0, -8, 20, 0), METASPR_ITEM(8, 0, 21, 0), METASPR_ITEM(-8, 8, 22, 0), METASPR_ITEM(8, 0, 23, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_retournement2[] = {
-    METASPR_ITEM(0, -8, 24, 0), METASPR_ITEM(0, 8, 26, 0),
+    METASPR_ITEM(0, -8, 24, 0), METASPR_ITEM(8, 0, 25, 0), METASPR_ITEM(-8, 8, 26, 0), METASPR_ITEM(8, 0, 27, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_retournement3[] = {
-    METASPR_ITEM(0, -8, 28, 0), METASPR_ITEM(0, 8, 30, 0),
+    METASPR_ITEM(0, -8, 28, 0), METASPR_ITEM(8, 0, 29, 0), METASPR_ITEM(-8, 8, 30, 0), METASPR_ITEM(8, 0, 31, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_retournement4[] = {
-    METASPR_ITEM(0, -8, 32, 0), METASPR_ITEM(0, 8, 34, 0),
+    METASPR_ITEM(0, -8, 32, 0), METASPR_ITEM(8, 0, 33, 0), METASPR_ITEM(-8, 8, 34, 0), METASPR_ITEM(8, 0, 35, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_retournement5[] = {
-    METASPR_ITEM(0, -8, 36, 0), METASPR_ITEM(0, 8, 38, 0),
+    METASPR_ITEM(0, -8, 36, 0), METASPR_ITEM(8, 0, 37, 0), METASPR_ITEM(-8, 8, 38, 0), METASPR_ITEM(8, 0, 39, 0),
     METASPR_TERM
 };
 
 const metasprite_t ship_retournement6[] = {
-    METASPR_ITEM(0, -8, 40, 0), METASPR_ITEM(0, 8, 42, 0),
+    METASPR_ITEM(0, -8, 40, 0), METASPR_ITEM(8, 0, 41, 0), METASPR_ITEM(-8, 8, 42, 0), METASPR_ITEM(8, 0, 43, 0),
     METASPR_TERM
 };
 
--- src/main.c
+++ src/main.c
@@ -7,50 +7,68 @@
 #include "../res/setSprites.h"
 
 #define SHIP_SPRITE     0
+#define SHOOT_SPRITE    4
 #define MAX_SHIP_SPEED  16
 #define SHIP_ACCEL      2
 
 joypads_t joypads;
 
+typedef struct ShipShoot ShipShoot;
+struct ShipShoot {
+    uint8_t active;
+    uint8_t sprite_num;
+    int8_t dir;
+    int16_t x,y;
+};
+
 void init_gfx() {
+    // Screen : 160x144
+
     // Load Background tiles and then map
     // set_bkg_data(0, 79u, dungeon_tiles);
     // set_bkg_tiles(0, 0, 32u, 32u, dungeon_mapPLN0);
 
-    SPRITES_8x16;
-    set_sprite_data(0, 44, Ship);
+    SPRITES_8x8;
+    set_sprite_data(0, 45, Ship);
+    set_sprite_tile(SHOOT_SPRITE, 44);
+    move_sprite(SHOOT_SPRITE, 10, 10);
 
 	// Turn the background map on to make it visible
     // SHOW_BKG;
     SHOW_SPRITES;
 }
 
-
 void main(void)
 {
-    uint16_t ShipX, ShipY;
-    int16_t SpdX, SpdY, direction, inclinaison;
-    uint8_t hiwater = 0;
+    uint16_t ShipX, ShipY, retourn_anim;
+    int16_t SpdX, SpdY;
+    int8_t direction, inclinaison;
+    uint8_t hiwater, retournement = 0;
+    ShipShoot ship_shoot;
 
 	init_gfx();
 
     joypad_init(1, &joypads);
 
     ShipX = ShipY = 64 << 4;
-    SpdX = SpdY = direction = inclinaison = 0;
+    SpdX = SpdY = inclinaison = retourn_anim = 0;
+    direction = 1;
 
     // Loop forever
     while(1) {
 		// Game main loop processing goes here
 
-        move_metasprite(ship_retournement[0], 0, 2, 20, 20);
-        move_metasprite(ship_retournement[1], 0, 4, 40, 22);
-        move_metasprite(ship_retournement[2], 0, 6, 60, 24);
-        move_metasprite(ship_retournement[3], 0, 8, 80, 26);
-        move_metasprite(ship_retournement[4], 0, 10, 100, 20);
-        move_metasprite(ship_retournement[5], 0, 12, 120, 40);
-
         joypad_ex(&joypads);
+
+        if (joypads.joy0 & J_A) {
+            EMU_printf("A PRESSED");
+            ship_shoot.dir = direction;
+            ship_shoot.active = 1;
+            ship_shoot.sprite_num = SHOOT_SPRITE;
+            ship_shoot.x = ShipX;
+            ship_shoot.y = ShipY;
+        }
+
         if (joypads.joy0 & J_UP) {
             SpdY -= SHIP_ACCEL;
             if (SpdY < - MAX_SHIP_SPEED) SpdY = - MAX_SHIP_SPEED;
@@ -69,28 +87,57 @@ void main(void)
         if (joypads.joy0 & J_LEFT) {
             SpdX -= SHIP_ACCEL;
             if (SpdX < - MAX_SHIP_SPEED) SpdX = - MAX_SHIP_SPEED;
-            direction = -1;
+            if ((direction == 1) && (!retournement)) {
+                // EMU_printf("Go LEFT");
+                retournement = 1;
+                retourn_anim = 0;
+            }
         } else if (joypads.joy0 & J_RIGHT) {
             SpdX += SHIP_ACCEL;
             if (SpdX > MAX_SHIP_SPEED) SpdX = MAX_SHIP_SPEED;
-            direction = 1;
+            if ((direction == -1) && (!retournement)) {
+                // EMU_printf("Go RIGHT");
+                retournement = 1;
+                retourn_anim = 0;
+            }
         }
         ShipX += SpdX, ShipY += SpdY;
 
-        if (direction >= 0) {
-            if (inclinaison >= 0)
-                hiwater = move_metasprite(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+        if (retournement) {
+            if (direction >= 0)
+                hiwater = move_metasprite(ship_retournement[retourn_anim >> 2], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+            else
+                hiwater = move_metasprite_vflip(ship_retournement[retourn_anim >> 2], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+            retourn_anim++;
+            if (retourn_anim>20) {
+                retournement = 0;
+                inclinaison = 16;
+                direction *= -1;
+            }
+        } else {
+            if (direction >= 0) {
+                if (inclinaison >= 0)
+                    hiwater = move_metasprite(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+                else {
+                    hiwater = move_metasprite_hflip(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4)+16);
+                }
+            }
             else {
-                EMU_printf("%d", inclinaison);
-                hiwater = move_metasprite_hflip(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4)+16);
+                if (inclinaison >= 0)
+                    hiwater = move_metasprite_vflip(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
+                else hiwater = move_metasprite_hvflip(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4)+16);
             }
         }
-        else {
-            if (inclinaison >= 0)
-                hiwater = move_metasprite_vflip(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4));
-            else hiwater = move_metasprite_hvflip(ship_meta[abs(inclinaison >> 2)], 0, SHIP_SPRITE, (ShipX >> 4), (ShipY >> 4)+16);
+        // hide_sprites_range(hiwater, 40);
+
+        if (ship_shoot.active) {
+            if (ship_shoot.dir >= 0) ship_shoot.x++;
+            else ship_shoot.x--;
+            if ((ship_shoot.x < 0) || (ship_shoot.x > 160))
+                ship_shoot.active = 0;
+            else
+                move_sprite(ship_shoot.sprite_num, ship_shoot.x, ship_shoot.y);
         }
-        // hide_sprites_range(hiwater, 40);  
 
 		// Done processing, yield CPU and wait for start of next frame
         wait_vbl_done();
