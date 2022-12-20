7a9907acc4e34332330b12831004b3ed4f400179
Julien Marusi <julien.marusi@bbee.fr>
2022-12-20 19:15:52 +0100

    boss

--- res/bak/title.h
+++ res/bak/title.h
@@ -11,17 +11,15 @@
 #define title_TILE_H 8
 #define title_WIDTH 160
 #define title_HEIGHT 144
-#define title_TILE_COUNT 164
+#define title_TILE_COUNT 360
 #define title_PALETTE_COUNT 1
 #define title_COLORS_PER_PALETTE 4
 #define title_TOTAL_COLORS 4
-#define title_MAP_ATTRIBUTES 0
 
 BANKREF_EXTERN(title)
 
 extern const palette_color_t title_palettes[4];
-extern const uint8_t title_tiles[2624];
+extern const uint8_t title_tiles[5760];
 
-extern const unsigned char title_map[360];
 
 #endif
--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@
 #include "mobs.h"
 
 #include "../res/title.h"
+#include "../res/boss.h"
+
 #include "../res/scene_tiles.h"
 #include "../res/scene1.h"
 
@@ -17,30 +19,24 @@ const palette_color_t cgb_pal_black[] = {RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_BL
 
 joypads_t joypads;
 Scene_t scene;
+extern unsigned char cEMPTY[]; 
 
 void titleScreen(void)
 {
-    // Set the screen to black via the palettes to hide the image draw
-    // if (_cpu == CGB_TYPE) {
-    //     set_bkg_palette(BKGF_CGB_PAL0, CGB_ONE_PAL, cgb_pal_black);
-    // } else {
-    //     BGP_REG = DMG_PALETTE(DMG_BLACK, DMG_BLACK, DMG_BLACK, DMG_BLACK);
-    // }
+	disable_interrupts();
+	DISPLAY_OFF;
+	HIDE_SPRITES;
+	HIDE_WIN;
+	HIDE_BKG;
+	DISPLAY_ON;
+	enable_interrupts();
 
-    // Display the image
-    // This will automatically switch to APA graphics mode
-    // and install it's start and mid-frame ISRs.
-    draw_image(title_tiles);
+    BGP_REG = DMG_PALETTE(DMG_WHITE, DMG_LITE_GRAY, DMG_DARK_GRAY, DMG_BLACK);
+     // Load up the tile data
+     set_bkg_data(0, title_TILE_COUNT, boss_tiles);
+     // Set screen x,y pos to 0,0 and draw the map 20,18(size of the screen)
+     set_bkg_tiles(0, 0, 20, 18, boss_map);
     SHOW_BKG;
-
-    // Then load the palettes at the start of a new frame
-    // wait_vbl_done();
-    // if (_cpu == CGB_TYPE) {
-    //     set_bkg_palette(BKGF_CGB_PAL0, CGB_ONE_PAL, title_palettes);
-    // } else {
-    //     BGP_REG = DMG_PALETTE(DMG_WHITE, DMG_LITE_GRAY, DMG_DARK_GRAY, DMG_BLACK);
-    // }
-
 
     // Loop forever
     while(1) {
@@ -56,36 +52,41 @@ void titleScreen(void)
 }
 
 void init_gfx() {
-    SPRITES_8x16;
-    DISPLAY_OFF;
-    VBK_REG = 1;
-    VBK_REG = 0;
+	SPRITES_8x16;
+	HIDE_SPRITES;
+	HIDE_BKG;
+	HIDE_WIN;
     set_bkg_data(0, 10, scene_tiles);
     setupScene(&scene, Scene1, Scene1Width, Scene1Height);
 
     initPlayer();
     initAliens();
 
-    SHOW_BKG;
-    SHOW_SPRITES;
-    DISPLAY_ON;
+	SHOW_BKG;
+	// SHOW_WIN;
+	SHOW_SPRITES;
+	DISPLAY_ON;
+}
+
+void none() {
 }
 
 void main(void)
 {
     titleScreen();
+    // DISPLAY_OFF;
 
-    // By default,
-    // the background and window TDT is located at addresses 0x8800-0x97FF,
-    // the sprite TDT at addresses 0x8000-0x8FFF,
-    // the BTM at addresses 0x9800-0x9BFF,
-    // and the WTM at addresses 0x9C00-0x9FFF
+    // STAT_REG = 0x45;
+    // LYC_REG = 0x00;
 
-    wait_vbl_done();
-LCDC_REG = LCDCF_BG8800;
-LCDC_REG = LCDCF_BG9800;
+	// disable_interrupts();
+    // add_LCD(none);
+    // enable_interrupts();
+    // set_interrupts(VBL_IFLAG | LCD_IFLAG);
 
+    // LCDC_REG = LCDCF_BG9800 | LCDCF_WIN9C00;
 	init_gfx();
+
     // gprintf("%x", get_mode());
 
     SCX_REG = 0; SCY_REG = 0;
